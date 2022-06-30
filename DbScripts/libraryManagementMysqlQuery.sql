3df5787b0c5aa92c9346699b082ad59533b1bd3d
sshukla27 <98134745+sshukla27@users.noreply.github.com>
2022-06-30 22:32:24 +0530

    Update libraryManagementMysqlQuery.sql

--- DbScripts/libraryManagementMysqlQuery.sql
+++ DbScripts/libraryManagementMysqlQuery.sql
@@ -1,21 +1,82 @@
-create database libraryManagementDb;
+create database LibraryManagement;
+use LibraryManagement;
+create table Book(
+bookId int auto_increment primary key, 
+bookName varchar(50),
+author varchar(50),
+review int,
+edition int,
+quantity int,
+genre varchar(50)
+);
+insert into Book(bookName,author,review,edition,quantity,genre) 
+values('Atomic Habit', 'Gery Chapman', 4, 2011, 8, 'Self-Help');
+update book set bookName = 'Atomic Habit2',author='Gery Chapman2',review=5,edition=2010,quantity=10,genre='Self-help2' where bookId = 1;
+select * from Book;
+drop table user;
+create table Users(
+userId int auto_increment primary key,
+userStatus bit,
+address varchar(250),
+contactNo long,
+regDate date,
+userName varchar(50),
+roleId int,
+emailId varchar(100),
+userPassword varchar(15)
+);
+insert into User(readerName,address,contactNo,email,registrationDate,roleId,pwd)
+values('Vishal Shah', 'Nandanagar, Indore', 5698564785, 'vishal@gmail.com', '0000');
 
-use libraryManagementDb;
-
-create table books
-(
-	bookId int auto_increment PRIMARY key,
-	bookName varchar(50) not null,
-	author varchar(50) not null,
-	review int,
-	edition int,
-	quantitiy int,
-	genre varchar(50) not null
+create table Genre(
+genreId int auto_increment primary key, 
+genreName varchar(1000)
+);
+insert into Genre(genreName)
+values(('Self-help'),('Fiction'),('Educational'),('Biography'),('Bestselling'),('Adventure'),('Crime/Thriller/Suspense'),('Classics'),('Comic'),('Graphic Novel
+'),('Philosophy'),('Political'),('Political'),('Romance'),('Sports'),('Travel'),('Autobiography')
+);
+select * from Genre;
+insert into Genre(genreName)values(('Fiction'));
+insert into Genre(genreName)values(('Educational'));
+insert into Genre(genreName)values(('Romance'));
+insert into Genre(genreName)values(('Travel'));
+select * from Genre;
+create table CustomerFeedback(
+readerID int auto_increment primary key,
+comments varchar(500), 
+date datetime,
+isApproved boolean,
+bookid int,
+review int);
+create table BookBorrow(
+userId int,
+	bookId int,
+	borrowDate datetime,
+	returnDate datetime
+    );
+create table Review(
+reviewId int auto_increment primary key,
+userId int,
+bookId int,
+stars int,
+comments varchar(500)
 );
 
-insert into books(bookName,author,review,edition,quantitiy,genre)
-values ('My E Book','Shirish',5,2022,100,'SciFi');
-insert into books(bookName,author,review,edition,quantitiy,genre)
-values ('My E Book2','Shirish',5,2022,100,'SciFi');
+create table Subscription(
+userId int,
+amount long, 
+dateOfSubscription datetime, 
+Validity da
+	
 
-select * from books;
+use LibraryManagement;
+userId int(11) AI PK 
+userStatus varchar(10) 
+registrationDate date 
+readerName varchar(50) 
+contactNo mediumtext 
+emailId varchar(25) 
+address varchar(100) 
+roleId int(11) 
+readerPassword varchar(15)
