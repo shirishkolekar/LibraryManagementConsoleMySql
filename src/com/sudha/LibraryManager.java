f85d2c70548e7f312955e75f4e97db865c07728b
shirishkolekar <61945126+shirishkolekar@users.noreply.github.com>
2022-07-03 23:04:29 +0530

    Signup call added

--- src/com/sudha/LibraryManager.java
+++ src/com/sudha/LibraryManager.java
@@ -10,9 +10,24 @@ public class LibraryManager {
 	public static void main(String[] args) {
 		int selectedOption = -1;
 		boolean loginAgain = false;
+		boolean signUpStatus = false;
 		System.out.print("Welcome to Lirbrary!");
 		do {
 			// login again - start
+			
+			do
+			{
+				System.out.print("\n\n\t Are you an existing user or new user? 1 - New User 2 - Existing User");
+				System.out.print("\n\n\t Your Choice : ");
+				if(sc.nextInt() == 1)
+				{
+					signUpStatus = Operation.signUp(sc);
+				}
+				else if(sc.nextInt() == 2)
+				{
+					signUpStatus = true;
+				}					
+			}while(!signUpStatus);	
 
 			System.out.print("Please login to proceed!");
 
